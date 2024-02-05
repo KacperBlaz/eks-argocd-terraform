e25c0a13eb32a268e51a4c23af69a914bd922ee3
Kacper Blaz <blazzkac@ul005068.eu.tieto.com>
2024-02-05 14:28:30 +0100

    ref: Terraform EKS cluster update kubeconfig

--- infra/modules/eks/eks.tf
+++ infra/modules/eks/eks.tf
@@ -60,7 +60,7 @@ resource "null_resource" "load_eks_kubeconfig" {
 
 data "local_file" "kubeconfig" {
   depends_on = [null_resource.load_eks_kubeconfig]
-  filename = "/root/.kube/config"
+  filename = "~/.kube/config"
 }
 
 
